577070dd79480e6f78e508b5f6628f3bb878f4c9
ruchi <ruchi@DESKTOP-KRV2GH1>
2022-03-08 22:34:27 +0000

    fix changes

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
  */
 const express = require("express");
 const router = express.Router();
+router.use(express.static(__dirname + '/views'));
+var path = require("path");
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Express' });
+    //res.render('index', { title: 'Express' });
+    res.sendFile((path.join(__dirname + '/../views/index.html')));
 });
 exports.default = router;
 //# sourceMappingURL=index.js.map
--- routes/index.ts
+++ routes/index.ts
@@ -3,9 +3,10 @@
  */
 import express = require('express');
 const router = express.Router();
-
+var path = require("path");
 router.get('/', (req: express.Request, res: express.Response) => {
-    res.render('index', { title: 'Express' });
+    //res.render('index', { title: 'Express' });
+    res.sendFile((path.join(__dirname + '/../views/index.html')));
 });
 
 export default router;
